40b22a27e5e3384714a2c8208121a832d038d5d2
sriravi-1987 <sriram.ravi1002gmail.com>
2024-08-12 14:54:56 +0000

    End of Section 9

--- FE/client/src/app/app.component.ts
+++ FE/client/src/app/app.component.ts
@@ -1,28 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
+export class AppComponent {
   title = 'Skinet';
-  private http = inject(HttpClient);
-  products: Product[]=[];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: response => this.products=response.data,
-      error: error => console.error(error),
-      complete: () => console.log('complete')
-    });
-  }
 }
---
+++ FE/client/src/app/features/shop/shop.component.ts
@@ -0,0 +1,100 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../core/services/shop.service';
+import { Product } from '../../shared/models/product';
+import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [ProductItemComponent, MatButton, MatIcon, MatMenu, MatSelectionList, 
+    MatListOption, MatMenuTrigger, MatPaginator, FormsModule],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss'
+})
+export class ShopComponent implements OnInit {
+  private shopSerice = inject(ShopService);
+  private dialogService = inject(MatDialog);
+
+  products?: Pagination<Product>;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ];
+
+  shopParams = new ShopParams();
+  pageSizeOptions = [5, 10, 15, 20];
+
+  ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  onSearchChange(){ 
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  initializeShop(){
+    this.shopSerice.getTypes();
+    this.shopSerice.getBrands();
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.shopSerice.getProducts(this.shopParams).subscribe({
+      next: response => this.products=response,
+      error: error => console.error(error)
+    });
+  }
+
+  handlePageEvent(event: PageEvent){
+    this.shopParams.pageNumber = event.pageIndex + 1;
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
+  }
+
+  openFiltersDialog()
+  {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data:{
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types
+      }
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if(result) 
+        {
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1;
+          this.getProducts();
+        }
+      }
+    });
+  }
+
+  onSortChange(event: MatSelectionListChange)
+  {
+    const selectedOption = event.options[0];
+    if(selectedOption.selected)
+    {
+      this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1;
+      this.getProducts();
+    }
+  }
+}
