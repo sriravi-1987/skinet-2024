d64d46002c394425376e1cab3fd78411d4d03e83
sriravi-1987 <sriram.ravi1002gmail.com>
2024-08-14 15:10:09 +0000

    End of Section 10

--- FE/client/src/app/core/services/shop.service.ts
+++ FE/client/src/app/core/services/shop.service.ts
@@ -48,6 +48,10 @@ export class ShopService {
     });
   }
 
+  getProduct(id:number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands(){
     if(this.brands.length > 0) return;
 
