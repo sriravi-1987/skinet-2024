be72c359c0ee59456d8fbffd58a7463454d7782f
sriravi-1987 <sriram.ravi1002gmail.com>
2024-08-20 12:06:37 +0000

    End of Sectio n12

--- FE/client/src/app/app.routes.ts
+++ FE/client/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
+import { CartComponent } from './features/cart/cart.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
+    {path: 'cart', component: CartComponent},
     {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
